8baa24afd4cf3c864c60cc8ab333c124a50cf2ed
Rene Sanchez <renesanchez@yahoo.com>
2017-07-25 14:48:36 -0500

    fixed spacing in urls

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -19,7 +19,7 @@ export class MessageService {
     const token = localStorage.getItem('token')
         ? '?token=' + localStorage.getItem('token')
         : '';
-    return this.http.post('https://angular2messageapp.herokuapp.com/message' + token, body, {headers: headers})
+    return this.http.post('http://angular2messageapp.herokuapp.com/message' + token, body, {headers: headers})
         .map((response: Response) => {
           const result = response.json();
           const message = new Message(
@@ -37,7 +37,7 @@ export class MessageService {
   }
 
   getMessages() {
-    return this.http.get('https://angular2messageapp.herokuapp.com/message')
+    return this.http.get('http://angular2messageapp.herokuapp.com/message')
         .map((response: Response) => {
           const messages = response.json().obj;
           let transformedMessages: Message[] = [];
@@ -68,7 +68,7 @@ export class MessageService {
     const token = localStorage.getItem('token')
         ? '?token=' + localStorage.getItem('token')
         : '';
-    return this.http.patch('https://angular2messageapp.herokuapp.com/message/' + message.messageId + token, body, {headers: headers})
+    return this.http.patch('http://angular2messageapp.herokuapp.com/message/' + message.messageId + token, body, {headers: headers})
         .map((response: Response) => response.json())
         .catch((error: Response) => {
           this.errorService.handleError(error.json());
@@ -81,7 +81,7 @@ export class MessageService {
     const token = localStorage.getItem('token')
         ? '?token=' + localStorage.getItem('token')
         : '';
-    return this.http.delete('https://angular2messageapp.herokuapp.com/message/' + message.messageId + token)
+    return this.http.delete('http://angular2messageapp.herokuapp.com/message/' + message.messageId + token)
         .map((response: Response) => response.json())
         .catch((error: Response) => {
           this.errorService.handleError(error.json());
